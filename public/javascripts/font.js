d05129cedfa9dbe1e0ff46523edea4f80ed5d5a8
Jonas <jonas@sketchin.ch>
2012-09-14 12:48:57 +0200

    modified stiles and minor bufixes

---
+++ public/javascripts/font.js
@@ -0,0 +1,14 @@
+// Place your application-specific JavaScript functions and classes here
+// This file is automatically included by javascript_include_tag :defaults
+WebFontConfig = {
+    google: { families: [ 'Leckerli+One::latin' ] }
+  };
+  (function() {
+    var wf = document.createElement('script');
+    wf.src = ('https:' == document.location.protocol ? 'https' : 'http') +
+      '://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js';
+    wf.type = 'text/javascript';
+    wf.async = 'true';
+    var s = document.getElementsByTagName('script')[0];
+    s.parentNode.insertBefore(wf, s);
+  })();
